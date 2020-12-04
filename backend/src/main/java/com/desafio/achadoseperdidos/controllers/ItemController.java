9dca1c4ac53cf05b9539639fc80a993dd69e0e9c
Wellisson Gomes <wellisson.cacho@ccc.ufcg.edu.br>
2020-12-04 17:56:17 -0300

    add ItemController

---
+++ backend/src/main/java/com/desafio/achadoseperdidos/controllers/ItemController.java
@@ -0,0 +1,28 @@
+package com.desafio.achadoseperdidos.controllers;
+
+import com.desafio.achadoseperdidos.dto.ItemDTO;
+import com.desafio.achadoseperdidos.entities.Item;
+import com.desafio.achadoseperdidos.services.ItemService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import javax.validation.Valid;
+import java.util.*;
+
+@RestController
+public class ItemController {
+    @Autowired
+    private ItemService itemService;
+
+    @GetMapping("/items")
+    public ResponseEntity<List<Item>> getAllItems(@RequestParam(required = false) Map<String, String> allRequestsParams) {
+        return new ResponseEntity<>(itemService.getAllItems(allRequestsParams), HttpStatus.OK);
+    }
+
+    @PutMapping("/item/{itemId}")
+    public ResponseEntity<Item> updateItem(@PathVariable Long itemId, @Valid @RequestBody ItemDTO itemDTO) {
+        return new ResponseEntity<>(itemService.updateItem(itemId, itemDTO), HttpStatus.OK);
+    }
+}
