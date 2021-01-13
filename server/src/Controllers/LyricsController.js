e32cae55987d234f060c2a6a2692edab7c0ff0f5
David Carmo <davidsc2812@gmail.com>
2021-01-13 10:22:13 -0300

    Variáveis descritivas
    Utilização de variáveis descritivas ao invés de apenas códigos nas urls de que requisição da API.

--- server/src/Controllers/LyricsController.js
+++ server/src/Controllers/LyricsController.js
@@ -1,5 +1,5 @@
 const axios = require('axios');
-
+const MUSIXMATCH_FINE_LINE_ID = '35618756';
 module.exports = 
 {
     async listLyricsIds(request, response)
@@ -7,7 +7,7 @@ module.exports =
         try
         {
             //get list of indexes of lyrics 
-            const listIndexLyrics = await axios.get(`${process.env.MUSIXMATCH_API_URL}album.tracks.get?album_id=35618756&page=1&page_size=12&apikey=${process.env.MUSIXMATCH_API_KEY}`);
+            const listIndexLyrics = await axios.get(`${process.env.MUSIXMATCH_API_URL}album.tracks.get?album_id=${MUSIXMATCH_FINE_LINE_ID}&page=1&page_size=12&apikey=${process.env.MUSIXMATCH_API_KEY}`);
             
             //convert list to an array with the lyrics ids 
             listIndexLyrics.data.message.body.track_list.map((lyricsId) => 
@@ -25,8 +25,8 @@ module.exports =
     async lyricById(request, response) 
     {
         const{id} = request.params; 
-        
-        const lyricById = await axios.get(`${process.env.MUSIXMATCH_API_URL}track.lyrics.get?track_id=${id}&apikey=${process.env.MUSIXMATCH_API_KEY}`); 
+        const MUSIXMATCH_TRACK_ID = id; 
+        const lyricById = await axios.get(`${process.env.MUSIXMATCH_API_URL}track.lyrics.get?track_id=${MUSIXMATCH_TRACK_ID}&apikey=${process.env.MUSIXMATCH_API_KEY}`); 
         
         const lyric = lyricById.data.message.body.lyrics.lyrics_body; //return the lyric text 
            
