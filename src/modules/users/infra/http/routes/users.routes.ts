e517f66302be36650944f0885a087254f82bdf85
juniorsilvaeng <juniorsilvacc@hotmail.com>
2022-01-18 03:44:19 -0300

    feat: user creation validation

--- src/modules/users/infra/http/routes/users.routes.ts
+++ src/modules/users/infra/http/routes/users.routes.ts
@@ -1,3 +1,4 @@
+import { celebrate, Segments, Joi } from 'celebrate';
 import { Router } from 'express';
 import { CreateUserController } from '../controllers/CreateUserController';
 
@@ -5,6 +6,16 @@ const usersRoutes = Router();
 
 const createUserController = new CreateUserController();
 
-usersRoutes.post('/', createUserController.handle);
+usersRoutes.post(
+  '/',
+  celebrate({
+    [Segments.BODY]: {
+      name: Joi.string().required(),
+      email: Joi.string().required().email(),
+      password: Joi.string().required().min(6).max(18),
+    },
+  }),
+  createUserController.handle,
+);
 
 export { usersRoutes };
--- src/shared/infra/http/server.ts
+++ src/shared/infra/http/server.ts
@@ -1,6 +1,7 @@
 import 'reflect-metadata';
 import express, { NextFunction, Request, Response } from 'express';
 import cors from 'cors';
+import { errors } from 'celebrate';
 import { routes } from './routes';
 import { AppError } from '@shared/errors/AppError';
 
@@ -11,8 +12,9 @@ const app = express();
 
 app.use(cors());
 app.use(express.json());
-
 app.use(routes);
+
+app.use(errors());
 
 app.use(
   // eslint-disable-next-line @typescript-eslint/no-unused-vars
