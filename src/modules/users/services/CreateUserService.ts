55e9f312abab4abd4bda2dd4210979b33de7ff7a
juniorsilvaeng <juniorsilvacc@hotmail.com>
2022-01-18 13:27:56 -0300

    feat: encrypting user password

--- src/modules/users/services/CreateUserService.ts
+++ src/modules/users/services/CreateUserService.ts
@@ -1,19 +1,31 @@
+import { AppError } from '@shared/errors/AppError';
 import { inject, injectable } from 'tsyringe';
 import { ICreateUserDTO } from '../domain/dtos/ICreateUserDTO';
 import { IUsersRepository } from '../domain/repositories/IUsersRepository';
+import { IHashProvider } from '../providers/models/IHashProvider';
 
 @injectable()
 class CreateUserService {
   constructor(
     @inject('UsersRepository')
     private usersRepository: IUsersRepository,
+    @inject('HashProvider')
+    private hashProvider: IHashProvider,
   ) {}
 
   async execute({ name, email, password }: ICreateUserDTO): Promise<void> {
+    const emailExists = await this.usersRepository.findByEmail(email);
+
+    if (emailExists) {
+      throw new AppError('Email address already used.');
+    }
+
+    const hashdPassword = await this.hashProvider.generateHash(password);
+
     await this.usersRepository.create({
       name,
       email,
-      password,
+      password: hashdPassword,
     });
   }
 }
--- src/shared/container/index.ts
+++ src/shared/container/index.ts
@@ -2,6 +2,7 @@ import { IUsersRepository } from '@modules/users/domain/repositories/IUsersRepos
 import { UsersRepository } from '@modules/users/infra/typeorm/repositories/UsersRepository';
 
 import { container } from 'tsyringe';
+import '@modules/users/providers';
 
 container.registerSingleton<IUsersRepository>(
   'UsersRepository',
