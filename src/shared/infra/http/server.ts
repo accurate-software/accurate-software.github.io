d79f0cc2e853cf06459d6f24c2c428bb57a94623
juniorsilvaeng <juniorsilvacc@hotmail.com>
2022-01-19 00:52:20 -0300

    chore: adding dotenv

--- src/shared/infra/http/server.ts
+++ src/shared/infra/http/server.ts
@@ -1,4 +1,5 @@
 import 'reflect-metadata';
+import 'dotenv/config';
 import express, { NextFunction, Request, Response } from 'express';
 import 'express-async-errors';
 import cors from 'cors';
@@ -37,5 +38,5 @@ app.use(
 );
 
 app.listen(3333, () => {
-  console.log('Server started on port 3333');
+  console.log(`Server started on port ${process.env.PORT}`);
 });
