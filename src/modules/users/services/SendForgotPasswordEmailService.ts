2d0edaf02fb75ab18a84f8d310029f323fde9f67
juniorsilvaeng <juniorsilvacc@hotmail.com>
2022-01-20 03:02:14 -0300

    feat: ethereal fake email service and tamplate handlebars

--- src/modules/users/services/SendForgotPasswordEmailService.ts
+++ src/modules/users/services/SendForgotPasswordEmailService.ts
@@ -1,3 +1,5 @@
+import path from 'path';
+import { EtherealMail } from '@config/mail/EtherealMail';
 import { AppError } from '@shared/errors/AppError';
 import { inject, injectable } from 'tsyringe';
 import { IUsersRepository } from '../domain/repositories/IUsersRepository';
@@ -23,9 +25,29 @@ class SendForgotPasswordEmailService {
       throw new AppError('User not found.', 404);
     }
 
-    const token = await this.userTokensRepository.generate(user.id);
+    const { token } = await this.userTokensRepository.generate(user.id);
 
-    console.log(token);
+    const forgotPasswordTemplate = path.resolve(
+      __dirname,
+      '..',
+      'views',
+      'forgot_password.hbs',
+    );
+
+    await EtherealMail.sendMail({
+      to: {
+        name: user.email,
+        email: user.email,
+      },
+      subject: '[API Achado e Perdidos] Recuperação de senha',
+      templateData: {
+        file: forgotPasswordTemplate,
+        variables: {
+          name: user.name,
+          link: `${process.env.APP_WEB_URL}/reset-password?token=${token}`,
+        },
+      },
+    });
   }
 }
 
