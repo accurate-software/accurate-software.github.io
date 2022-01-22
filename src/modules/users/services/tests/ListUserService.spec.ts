9d82b950e545350d421463b0e4343056b62d97a6
juniorsilvaeng <juniorsilvacc@hotmail.com>
2022-01-22 18:30:55 -0300

    test: ensure that the listing of all users returns 200

---
+++ src/modules/users/services/tests/ListUserService.spec.ts
@@ -0,0 +1,33 @@
+import { UsersFakeRepository } from '../../domain/repositories/fakes/UsersFakeRepository';
+import { HashProviderFakeRepository } from '../../providers/fakes/HashProviderFakeRepository';
+import { CreateUserService } from '../CreateUserService';
+import { ListUserService } from '../ListUserService';
+
+let listUserService: ListUserService;
+let createUserService: CreateUserService;
+let usersFakeRepository: UsersFakeRepository;
+let hashProviderFakeRepository: HashProviderFakeRepository;
+
+describe('List Users', () => {
+  beforeEach(() => {
+    usersFakeRepository = new UsersFakeRepository();
+    hashProviderFakeRepository = new HashProviderFakeRepository();
+    createUserService = new CreateUserService(
+      usersFakeRepository,
+      hashProviderFakeRepository,
+    );
+    listUserService = new ListUserService(usersFakeRepository);
+  });
+
+  it('should be able to list all users', async () => {
+    const user = await createUserService.execute({
+      name: 'Nome Test',
+      email: 'test@test.com',
+      password: '123456',
+    });
+
+    const users = await listUserService.execute();
+
+    expect(users).toEqual([user]);
+  });
+});
