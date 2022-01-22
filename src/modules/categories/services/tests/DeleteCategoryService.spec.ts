e98bec0c7a5680e8e8469d8f7665e384ad05dc63
juniorsilvaeng <juniorsilvacc@hotmail.com>
2022-01-22 03:25:02 -0300

    test: ensure category is excluded

---
+++ src/modules/categories/services/tests/DeleteCategoryService.spec.ts
@@ -0,0 +1,27 @@
+import { CategoriesFakeRepository } from '../../domain/repositories/fakes/CategoriesFakeRepository';
+import { CreateCategoryService } from '../CreateCategoryService';
+
+let createCategoryService: CreateCategoryService;
+let categoriesFakeRepository: CategoriesFakeRepository;
+
+describe('Delete Category', () => {
+  beforeEach(() => {
+    categoriesFakeRepository = new CategoriesFakeRepository();
+    createCategoryService = new CreateCategoryService(categoriesFakeRepository);
+  });
+
+  it('should be able to delete a category', async () => {
+    const category = await createCategoryService.execute({
+      name: 'Category',
+      description: 'Category Description',
+    });
+
+    expect(category).toHaveProperty('id');
+
+    const deleteCategory = await categoriesFakeRepository.findByDelete(
+      category.id,
+    );
+
+    expect(deleteCategory).toBeUndefined();
+  });
+});
