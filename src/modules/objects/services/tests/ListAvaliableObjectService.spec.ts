0d86d21035b3c33d1bd91e7cc1b51c613cdf8372
juniorsilvaeng <juniorsilvacc@hotmail.com>
2022-01-22 17:39:15 -0300

    test: ensure that listing all objects accessible by category returns 200

--- src/modules/objects/services/tests/ListAvaliableObjectService.spec.ts
+++ src/modules/objects/services/tests/ListAvaliableObjectService.spec.ts
@@ -45,4 +45,19 @@ describe('List Objetcs', () => {
 
     expect(objetcs).toEqual([objectAvailable]);
   });
+
+  it('should be able to list all available objetcs by category', async () => {
+    const objectAvailable = await createObjectService.execute({
+      name: 'Object',
+      comments: 'Comments',
+      type: TypeEnum.Found,
+      category_id: 'category_id',
+    });
+
+    const objetcs = await listAvaliableObject.execute({
+      category_id: 'category_id',
+    });
+
+    expect(objetcs).toEqual([objectAvailable]);
+  });
 });
