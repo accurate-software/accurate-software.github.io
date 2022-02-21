687b11788329063edffa0cd23811c51f6a20d8a1
taylsonmartinez <taylson.martinez@gmail.com>
2022-02-20 21:33:46 -0300

    handleIllegalArgumentException

--- api/src/main/java/com/taylsonmartinez/api/controllers/ApplicationControllerAdvice.java
+++ api/src/main/java/com/taylsonmartinez/api/controllers/ApplicationControllerAdvice.java
@@ -7,6 +7,7 @@ import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.web.method.annotation.MethodArgumentTypeMismatchException;
 
 import java.util.List;
 import java.util.stream.Collectors;
@@ -29,6 +30,13 @@ public class ApplicationControllerAdvice {
                 .map(erro -> erro.getDefaultMessage())
                 .collect(Collectors.toList());
         return new ApiErrors(errors);
+    }
+
+    @ExceptionHandler(IllegalArgumentException .class)
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    public ApiErrors handleIllegalArgumentException(IllegalArgumentException  ex) {
+        String mensagemErro = ex.getMessage();
+        return new ApiErrors(mensagemErro);
     }
 
 }
